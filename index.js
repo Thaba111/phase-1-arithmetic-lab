b3d173c1db60a901952baddab0b58d72506a4b97
Edith Thaba <writersherry5@gmail.com>
2023-12-13 13:29:30 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+// Write your code here
+//Create a variable called multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
+
+//Create a variable called random
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random); // Output: Random integer greater than 0
+
+//Create a variable called mod
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+//=>4
+
+//Create a variable called max
+const max =  Math.max(1,2,3,7,14,16,20)
+
+
+
